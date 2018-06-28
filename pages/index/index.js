93d42696b57880b4caec71e3e1548e09c287cb1b
xiyanbox <13735800742@163.com>
2018-06-28 22:08:04 +0800

    persom

--- pages/index/index.js
+++ pages/index/index.js
@@ -24,10 +24,7 @@ Page({
     })
   },
   onLoad: function () {
-    var that = this
-
-
-
+    var that = this;
     // 实例化API核心类
              qqmapsdk = new QQMapWX({
       key: 'CKOBZ-V6R3I-CC2GL-5IENL-BC6HF-BQF6Z'        
@@ -64,32 +61,7 @@ Page({
     })
 
 
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    
   },
   onShow: function () {
     // 调用接口
